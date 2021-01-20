aefe471e044e08746a644e1b6d73addfebd94cdb
inespl <ines.pl@ua.pt>
2021-01-20 22:56:48 +0000

    updates

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/proj-tpw-g5'));
+app.use(express.static(__dirname + '/dist/websit'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+ '/dist/proj-tpw-g5/index.html'));});
+res.sendFile(path.join(__dirname+ '/dist/website/index.html'));});
 app.listen(process.env.PORT || 8080);
